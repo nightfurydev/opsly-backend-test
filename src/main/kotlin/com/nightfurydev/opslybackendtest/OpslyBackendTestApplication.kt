0b56b12da27805beb24baf78c95a7acbb2e5fa32
Alexey Kleschikov <alexey.kleschikov@gmail.com>
2020-02-20 09:58:07 +0300

    Project initialized with Spring Initializr.

---
+++ src/main/kotlin/com/nightfurydev/opslybackendtest/OpslyBackendTestApplication.kt
@@ -0,0 +1,11 @@
+package com.nightfurydev.opslybackendtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpslyBackendTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpslyBackendTestApplication>(*args)
+}
